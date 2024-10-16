357701ab181dac53ff96763f143b642b89dc8d55
TomChen <voyager.teng@gmail.com>
2024-10-17 01:01:51 +0800

    專案初始化

---
+++ src/main/java/com/example/currencyPrice/CurrencyPriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currencyPrice/CurrencyPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyPrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
